2ed745ef47fd1817ad98829233c669f88908e839
sivaji085 <marrisivaji05@gmail.com>
2026-08-21 10:58:55 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-tetris-sivaji-730" # Replace with your actual S3 bucket name
+    bucket = "devsecops-tetris-sivaji-731" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
